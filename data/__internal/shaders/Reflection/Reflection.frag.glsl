101fd849819b4b5beeceb8ba09807b96de1356da
Timur Gafarov <gecko0307@gmail.com>
2026-05-10 12:31:54 +0300

    Reflection blur

--- data/__internal/shaders/Reflection/Reflection.frag.glsl
+++ data/__internal/shaders/Reflection/Reflection.frag.glsl
@@ -12,10 +12,22 @@ layout(location = 0) in vec2 texCoords;
 
 layout(location = 0) out vec4 outColor;
 
+vec3 blurReflection(vec2 uv)
+{
+    vec2 texel = 1.0 / ubo.resolution.xy;
+    vec3 sum = texture(reflectionBuffer, uv).rgb * 0.4;
+    sum += texture(reflectionBuffer, uv + vec2(texel.x, 0.0)).rgb * 0.15;
+    sum += texture(reflectionBuffer, uv - vec2(texel.x, 0.0)).rgb * 0.15;
+    sum += texture(reflectionBuffer, uv + vec2(0.0, texel.y)).rgb * 0.15;
+    sum += texture(reflectionBuffer, uv - vec2(0.0, texel.y)).rgb * 0.15;
+    return sum;
+}
+
 void main()
 {
     vec3 original = texture(colorBuffer, texCoords).rgb;
     vec4 reflection = texture(reflectionBuffer, texCoords);
-    vec3 color = mix(original, reflection.rgb, reflection.a);
+    vec3 blurredReflection = blurReflection(texCoords);
+    vec3 color = mix(original, blurredReflection, reflection.a);
     outColor = vec4(color, 1.0);
 }
