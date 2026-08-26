17e144d61dd07554848b8acd5d119c1344fdb212
Timur Gafarov <gecko0307@gmail.com>
2026-08-26 15:04:39 +0300

    Update ddoc

--- src/dagon/render/frametask.d
+++ src/dagon/render/frametask.d
@@ -6,6 +6,12 @@ import dagon.core.sdl3;
 import dagon.core.time;
 import dagon.render.renderer;
 
+/*
+ * FrameTask allows to insert custom GPU commands
+ * to the command buffer in the renderer.
+ * You can use it, for example, to update textures
+ * or meshes in real time.
+ */
 abstract class FrameTask: Owner
 {
     bool active = true;
